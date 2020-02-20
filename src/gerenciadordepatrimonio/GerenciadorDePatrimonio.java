fb619b611454dadc083ba40ea088be9c13c56266
Gustavo-hen <56658295+Gustavo-hen@users.noreply.github.com>
2020-02-20 20:05:31 -0300

    Update GerenciadorDePatrimonio.java
    commit web

--- src/gerenciadordepatrimonio/GerenciadorDePatrimonio.java
+++ src/gerenciadordepatrimonio/GerenciadorDePatrimonio.java
@@ -15,7 +15,7 @@ public class GerenciadorDePatrimonio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // teste teste
     }
     
 }
