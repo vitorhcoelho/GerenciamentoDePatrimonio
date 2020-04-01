d37a2d856f34a091994dfee1b8d4f64d0b924a1b
gusta <gusta@LAPTOP-GN62B9KQ>
2020-03-31 21:36:43 -0300

    Commit Gustavo Login e menu (ok) Campus ok

--- src/gerenciadordepatrimonio/AmbienteDAO.java
+++ src/gerenciadordepatrimonio/AmbienteDAO.java
@@ -5,10 +5,43 @@
  */
 package gerenciadordepatrimonio;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gusta
  */
 public class AmbienteDAO {
+
+    private Ambiente[] ambs = new Ambiente[100];
+    private int qtdAmb = 0;
+    private int id = 0;
+    Scanner in = new Scanner(System.in);
     
+    public void insereAmbiente(){
+        
+    }
+
+    public int vagaAmbiente() {
+        for (int x = 0; x < ambs.length; x++) {
+            if (ambs[x] == null) {
+                return x;
+            } else {
+
+            }
+        }
+        return -1;
+    }
+
+    public boolean setAmbiente(Ambiente amb) {
+        int pos = vagaAmbiente();
+        if (pos == -1) {
+            System.out.println("\nLista de Campus Cheia\n");
+            return false;
+        } else {
+            ambs[pos] = amb;
+            this.qtdAmb++;
+            return true;
+        }
+    }
 }
