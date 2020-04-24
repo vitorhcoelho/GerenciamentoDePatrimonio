8356a66d6585c927b6aacc541c76e8ab93a33ec9
gusta <gusta@LAPTOP-GN62B9KQ>
2020-04-23 21:52:54 -0300

    Merge origin/master-branch
    Conflicts:
    	src/gerenciadordepatrimonio/FazTudo.java
    	src/gerenciadordepatrimonio/MovimentoItem.java
    	src/gerenciadordepatrimonio/MovimentoItemDAO.java

--- src/gerenciadordepatrimonio/MovimentoAmbienteDAO.java
+++ src/gerenciadordepatrimonio/MovimentoAmbienteDAO.java
@@ -10,5 +10,38 @@ package gerenciadordepatrimonio;
  * @author gusta
  */
 public class MovimentoAmbienteDAO {
+    private MovimentoAmbiente [] movAmb = new MovimentoAmbiente[100];
+    private int id = 0;
+    
+    
+    public int geraId(){
+        this.id++;
+        return id;
+    }
+    
+     public int vagaMovAmb() {
+        for (int x = 0; x < movAmb.length; x++) {
+            if (movAmb[x] == null) {
+                return x;
+            } else {
 
+            }
+        }
+        return -1;
+    }
+
+    public boolean setMovAmb(MovimentoAmbiente mov) {
+        int pos = vagaMovAmb();
+        if (pos == -1) {
+            System.out.println("\nLista de Campus Cheia\n");
+            return false;
+        } else {
+            movAmb[pos] = mov;
+            return true;
+        }
+    }
+    
+    public void insereMovAmb(){
+        
+    }
 }
